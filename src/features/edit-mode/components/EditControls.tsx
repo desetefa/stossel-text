79d25a79f449ae0f5901500a68d36afea964b644
Dan <danndestefano@gmail.com>
2026-02-26 16:25:25 -0500

    added onlock

--- src/features/edit-mode/components/EditControls.tsx
+++ src/features/edit-mode/components/EditControls.tsx
@@ -7,6 +7,12 @@ export function EditControls({ onLock, onCancel }: EditControlsProps) {
   return (
     <div className="edit-mode-controls">
       <button
+        onClick={onLock}
+        className="btn btn-primary btn-small"
+      >
+        Lock
+      </button>
+      <button
         onClick={onCancel}
         className="btn btn-secondary btn-small"
       >
